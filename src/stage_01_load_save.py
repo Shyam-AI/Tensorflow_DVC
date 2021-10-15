108628655333e29d71268957e39816b3b2cea462
Shyam1805 <shyamgokulam42@gmail.com>
2021-10-15 20:56:20 +0530

    Stage-02 added and parameters added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument("--config", "-c", default="config/config.yaml")
-
+ 
     parsed_args = args.parse_args()
 
     try:
