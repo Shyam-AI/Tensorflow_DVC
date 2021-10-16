b0f2ad8f25c2f500d9536183296ba7e5fd749de6
Shyam1805 <shyamgokulam42@gmail.com>
2021-10-16 19:44:52 +0530

    stage-02 added and parameters updated

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,10 +1,11 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.models import get_VGG_16_model, prepare_model
 import argparse
 import pandas as pd
 import os
 from tqdm import tqdm     #prints the progress bar when we run the for loop
 import logging
+import io
 
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
@@ -30,6 +31,22 @@ def prepare_base_model(config_path, params_path):
 
     model = get_VGG_16_model(input_shape= params["IMAGE_SIZE"], model_path=base_model_path)
 
+    model = prepare_model(model,
+    CLASSES = params["CLASSES"],
+    freeze_all = True,
+    freeze_till=None,
+    learning_rate =params["LEARNING_RATE"])
+
+    updated_base_model = os.path.join(base_model_dir_path, artifacts["UPDATED_BASE_MODEL"])
+
+    def _log_model_summary(full_model):
+        with io.StringIO() as stream:
+            full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f"full model summary: \n{_log_model_summary(model)}")
+    model.save(updated_base_model)
 
 
 if __name__ == '__main__':
