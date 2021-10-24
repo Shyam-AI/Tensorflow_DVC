3fc748d5e5b2fd64533d456cafae44b50ccb0a6c
Shyam-AI <shyamdl2803@gmail.com>
2021-10-24 18:53:34 +0530

    fixed the bug while creating a directory

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -21,7 +21,7 @@ def create_and_save_checkpoint_callbacks(call_back_dir, checkpoint_dir):
     checkpoint_file_path = os.path.join(checkpoint_dir, "ckpt_model.h5")
     checkpoint_callbacks = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_file_path, save_best_only= True)
 
-    ckpt_callbacks_filepath = os.path.join(call_back_dir, "checkpint_cb.cb")
+    ckpt_callbacks_filepath = os.path.join(call_back_dir, "checkpoint_cb.cb")
     joblib.dump(checkpoint_callbacks, ckpt_callbacks_filepath)
 
-    logging.info(f"Tensorboard callback is being saved at {ckpt_callbacks_filepath}")
+    logging.info(f"Checkpoints callback is being saved at {ckpt_callbacks_filepath}")
